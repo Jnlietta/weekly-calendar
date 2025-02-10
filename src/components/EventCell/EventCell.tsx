ead6dcfe7cb866e23e86e259f80bd29d0975220e
Julia Glinka <julia.w.glinka@gmail.com>
2025-02-10 20:49:54 +0100

    Add component EventCell

---
+++ src/components/EventCell/EventCell.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EventCell = () => {
+    return (
+        <div className="event-cell bg-primary text-white rounded-lg p-1">
+        <p className="text-sm">Spotkanie z klientem</p>
+        </div>
+    );
+    };
+
+export default EventCell;
