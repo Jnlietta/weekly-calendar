8cd7484ce18c8d914367559c6820011a4631031e
Julia Glinka <julia.w.glinka@gmail.com>
2025-03-27 15:02:33 +0100

    Add Provider with store in index.tsx and add redux/store.ts

---
+++ src/redux/store.ts
@@ -0,0 +1,16 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+
+// import reducers
+
+
+// combine reducers
+const rootReducer = combineReducers({
+
+});
+
+const store = createStore(
+  rootReducer,
+
+);
+
+export default store;
